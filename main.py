897a1b543fd614201a20161c82371ee923953e68
kendallli587 <kendallli587@gmail.com>
2022-07-22 12:19:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 # Distance Calculator Using Python
+
 import math
-
-
-def calc_distance(x1, y1, x2, y2):
-    math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
 
 # INPUT
 x1 = int(input("Please enter the x-value of the first point: "))
@@ -13,13 +9,7 @@ x2 = int(input("Please enter the x-value of the second point: "))
 y2 = int(input("Please enter the y-value of the second point: "))
 
 # PROCESS
-
-
-def calc_distance(x1, y1, x2, y2):
-    math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
-
-distance = calc_distance(x1, y1, x2, y2)
+distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 # OUTPUT
 print("The distance between the two points is " + str(distance) + ".")
